7b38e5a06c56d2ae66d761903a649a1ec4e0a42a
stankovics <stankovicnstefan@gmail.com>
2022-04-20 20:01:41 -0500

    onMouse()

--- js/index.js
+++ js/index.js
@@ -18,4 +18,15 @@ const onMouseMove = e => {
   const { clientX, clientY } = e;
   const x = Math.round((clientX / window.innerWidth) * 100);
   const y = Math.round((clientY / window.innerHeight) * 100);
+
+  gsap.to(hero, {
+    '--x': `${x}%`,
+    '--y': `${y}%`,
+    duration: 0.3,
+    ease: 'sine.out',
+  });
 };
+// Text animation
+Splitting();
+
+// Set initial text styles (before animation)
