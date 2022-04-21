2284d40fdd1dc01d5e377567124c62600a7cda2d
stankovics <stankovicnstefan@gmail.com>
2022-04-20 20:35:42 -0500

    finished

--- dist/index.5d9dacde.js
+++ dist/index.5d9dacde.js
@@ -542,7 +542,34 @@ const onMouseMove = (e)=>{
     });
 };
 // Text animation
-_splittingDefault.default(); // Set initial text styles (before animation)
+_splittingDefault.default();
+// Set initial text styles (before animation)
+_gsapDefault.default.set('.hero--primary .char', {
+    opacity: 0,
+    y: 25
+});
+// Timeline
+const tl = _gsapDefault.default.timeline({
+    delay: 1
+});
+tl.to('.hero--primary .char', {
+    opacity: 1,
+    y: 0,
+    duration: 0.75,
+    stagger: 0.1
+}).to(hero, {
+    '--maskSize1': '20%',
+    duration: 0.5,
+    ease: 'back.out(2)'
+}).to(hero, {
+    '--maskSize2': '28%',
+    '--maskSize3': 'calc(28% + 0.1rem)',
+    duration: 0.5,
+    delay: 0.3,
+    ease: 'back.out(2)'
+}).then(()=>{
+    window.addEventListener('mousemove', _lodashThrottleDefault.default(onMouseMove, 30));
+});
 
 },{"gsap":"fPSuC","splitting":"77jB6","lodash.throttle":"bGJVT","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"fPSuC":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
